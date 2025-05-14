78dc87c9a3c15e3c7b02f515cd51afd72aa5b173
GrandeGH <georgesgrande96@gmail.com>
2025-05-14 11:06:07 +0200

    1er commit

---
+++ app/Http/Controllers/LibrairieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Librairie;
+use App\Http\Requests\StoreLibrairieRequest;
+use App\Http\Requests\UpdateLibrairieRequest;
+
+class LibrairieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLibrairieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Librairie $librairie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Librairie $librairie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLibrairieRequest $request, Librairie $librairie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Librairie $librairie)
+    {
+        //
+    }
+}
