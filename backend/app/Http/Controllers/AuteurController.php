0125d0ac7d05685a237593cee99edae71f2032fa
GrandeGH <georgesgrande96@gmail.com>
2025-05-14 14:03:27 +0200

    5e commit ajout model Catagorie et Auteur

---
+++ backend/app/Http/Controllers/AuteurController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Auteur;
+use App\Http\Requests\StoreAuteurRequest;
+use App\Http\Requests\UpdateAuteurRequest;
+
+class AuteurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAuteurRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Auteur $auteur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Auteur $auteur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAuteurRequest $request, Auteur $auteur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Auteur $auteur)
+    {
+        //
+    }
+}
